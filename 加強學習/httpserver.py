9508affef330495c869d2550083ad8ee02bdac86
hgalytoby <hgalytoby@gmail.com>
2021-08-01 02:23:52 +0800

    加強學習

--- 加強學習/httpserver.py
+++ 加強學習/httpserver.py
@@ -10,11 +10,11 @@ if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
     port = 8000
-server_address = ('192.168.50.8', port)
+server_address = ('0.0.0.0', port)
 
 HandlerClass.protocol_version = Protocol
 httpd = ServerClass(server_address, HandlerClass)
 
 sa = httpd.socket.getsockname()
-print("Serving HTTP on", sa[0], "port", sa[1], "...", 'http://192.168.50.8:8000')
+print("Serving HTTP on", sa[0], "port", sa[1], "...", 'http://0.0.0.0:8000')
 httpd.serve_forever()
