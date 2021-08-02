a518a1ce9af7a04a25d3cda894bd5f1a05538482
hgalytoby <hgalytoby@gmail.com>
2021-08-02 22:55:30 +0800

    加強學習

--- 加強學習/test.py
+++ 加強學習/test.py
@@ -1,1 +1,24 @@
-import this
+class Base:
+    Str = ""
+    Int = 0
+    List = []
+    Dict = {}
+
+
+class A(Base):
+    mood = 'Happy'
+
+
+class B(Base):
+    mood = 'Devil'
+
+
+print(A.__dict__)
+a = A()
+
+# a.__dict__.update({
+#     'A': 'a',
+#     'B': 'b'
+# })
+# print(a.__dict__)
+
