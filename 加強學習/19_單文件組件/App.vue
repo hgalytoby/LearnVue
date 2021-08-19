a598e4db297b40992be977ec38cbe91f77bd67fc
hgalytoby <hgalytoby@gmail.com>
2021-08-19 23:01:58 +0800

    加強學習

---
+++ 加強學習/19_單文件組件/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+    // 導入組件
+    import School from "./School";
+    import Student from "./Student";
+
+    export default {
+        name: "App",
+        components: {
+            School,
+            Student
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 加強學習/19_單文件組件/Student.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>名字: {{ name }}</h2>
+        <h2>年齡: {{ age }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Student",
+        data(){
+            return {
+                name: 'dudulu',
+                age: 18
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
