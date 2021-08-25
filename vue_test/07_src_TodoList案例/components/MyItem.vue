27217bafd8d5b95bb4922ae28bc5179be3bf9d5f
hgalytoby <hgalytoby@gmail.com>
2021-08-25 22:58:03 +0800

    加強學習。

---
+++ vue_test/07_src_TodoList案例/components/MyItem.vue
@@ -0,0 +1,73 @@
+<template>
+    <li>
+        <label>
+            <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
+            <!-- 以下方法也能實現，但是因為 Vue 不允許更改 props 值，Vue 是淺層監測，所以對象內的值被更改不會被發現。 -->
+            <!--<input type="checkbox" v-model="todo.done"/>-->
+            <span>{{ todo.title }}</span>
+        </label>
+
+        <button class="btn btn-danger" @click="handleDelete(todo.id)">刪除</button>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "MyItem",
+        // 接收參數,
+        methods: {
+            handleCheck(todoId) {
+                this.checkTodo(todoId)
+            },
+            handleDelete(todoId){
+                if (confirm('確定刪除嗎?')){
+                    this.deleteTodo(todoId)
+                }
+                // console.log(todoId)
+            },
+        },
+        props: {
+            todo: Object,
+            checkTodo: Function,
+            deleteTodo: Function,
+        }
+    }
+</script>
+
+<style scoped>
+    /*item*/
+    li {
+        list-style: none;
+        height: 36px;
+        line-height: 36px;
+        padding: 0 5px;
+        border-bottom: 1px solid #ddd;
+    }
+    li label {
+        float: left;
+        cursor: pointer;
+    }
+    li label li input {
+        vertical-align: middle;
+        margin-right: 6px;
+        position: relative;
+        top: -1px;
+    }
+    li button {
+        float: right;
+        display: none;
+        margin-top: 3px;
+    }
+    li:before {
+        content: initial;
+    }
+    li:last-child {
+        border-bottom: none;
+    }
+    li:hover {
+        background-color: #dddddd;
+    }
+    li:hover button{
+        display: block;
+    }
+</style>
--- vue_test/README.md
+++ vue_test/README.md
@@ -111,3 +111,34 @@
         - npm view less-loader versions
         - Vue 腳手架用 webpack 4.46.0，所以選擇 less-loader 6 ~ 7版本。
             - `npm i less-loader@7` 預設裝 7 最新。
+            
+## 總結 TodoList 案例
+- 1.組件化編碼流程: 
+    - (1).拆分靜態組件: 組件要按照功能點拆分，命名不要與 html 元素衝突。
+    - (2).實現動態組件: 考慮好資料的存放位置，資料是一個組件在用，還是一些組件在用: 
+        - (1).一個組件在用: 放在組件自身即可。
+        - (2).一些組件在用: 放在他們共同的父組件上(<span style="color:red">狀態提升</span>)。
+    - (3).實現交互: 從綁定事件開始。
+- 2.props 適用於: 
+    - (1).父組件 ==> 子組件 通信
+    - (2).子組件 ==> 父組件 通信(要求父先給子一個函數)
+- 3.使用 v-model 時要切記: v-model 綁定的值不能是 props 傳過來的值，因爲 props 是不可以修改的！
+- 4.props 傳過來的若是對象類型的值，修改對象中的屬性時 Vue 不會報錯，但不推薦這樣做。
+
+## webStorage
+- 1.存儲內容大小一般支持 5MB 左右（不同瀏覽器可能還不一樣）
+- 2.瀏覽器端通過 Window.sessionStorage 和 Window.localStorage 屬性來實現本地存儲機制。
+- 3.相關API: 
+    - 1. ```xxxxxStorage.setItem('key', 'value')```
+        - 該方法接受一個鍵和值作爲參數，會把鍵值對添加到存儲中，如果鍵名存在，則更新其對應的值。
+    - 2. ```xxxxxStorage.getItem('person')```
+        - 該方法接受一個鍵名作爲參數，返回鍵名對應的值。
+    - 3. ```xxxxxStorage.removeItem('key')```
+        - 該方法接受一個鍵名作爲參數，並把該鍵名從存儲中刪除。
+    - 4. ```xxxxxStorage.clear()```
+        - 該方法會清空存儲中的所有資料。
+- 4. 備註: 
+    - 1.SessionStorage 存儲的內容會隨着瀏覽器視窗關閉而消失。
+    - 2.LocalStorage 存儲的內容，需要手動清除才會消失。
+    - 3.```xxxxxStorage.getItem(xxx)```如果 xxx 對應的 value 獲取不到，那麼 getItem 的返回值是null。
+    - 4.```JSON.parse(null)```的結果依然是 null。
